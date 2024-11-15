8ab3ff345fd10382b98b34d9ee5d33e4935b26b0
임형구 <damansa1@naver.com>
2024-11-15 10:39:32 +0900

    커스텀컨트롤 작성

---
+++ WpfApp1.CustomControls/CustomButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.CustomControls;
+public class CustomButton : Button
+{
+    public static readonly DependencyProperty IsHighlightedProperty =
+        DependencyProperty.Register(nameof(IsHighlighted), typeof(bool),
+            typeof(CustomButton), new PropertyMetadata(false));
+
+    public bool IsHighlighted
+    {
+        get => (bool)GetValue(IsHighlightedProperty);
+        set => SetValue(IsHighlightedProperty, value);
+    }
+
+    static CustomButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomButton),
+            new FrameworkPropertyMetadata(typeof(CustomButton)));
+    }
+}
